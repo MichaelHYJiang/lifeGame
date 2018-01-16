de5a29030f61c33259287ee1aedf02a98853d914
Haiyang Jiang <45019156@qq.com>
2018-01-16 02:44:21 -0800

    add global variable

--- life.py
+++ life.py
@@ -1,9 +1,13 @@
 import numpy as np
 
+cols = 10
+rows = 10
+grid = np.array([])
+
 def make2DArray(cols, rows):
     array = np.random.rand(rows, cols)
     array = np.array(array * 2, dtpye = 'uint8')
     return array
 
 if __name__ == '__main__':
-    grid = make2DArray(10, 10)
+    grid = make2DArray(cols, rows)
